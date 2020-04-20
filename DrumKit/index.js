5cec940f38b5041c6924a7c770242fdfc5481a26
nemus <nemanjadjekic93@gmail.com>
2020-04-20 16:46:11 +0200

    Added animations and did some refactoring

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -2,12 +2,24 @@ var numOfDrumBtns = document.querySelectorAll(".drum").length;
 
 // Detecting Button Press
 for (var i = 0; i < numOfDrumBtns; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", btnSwitcher);
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var btnInnerHTML = this.innerHTML;
+
+    makeSound(btnInnerHTML);
+
+    btnAnimation(btnInnerHTML);
+  });
 }
 
-function btnSwitcher() {
-  var btnSwitch = this.innerHTML;
-  switch (btnSwitch) {
+// Detecting Keyboard Press
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+
+  btnAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
@@ -48,49 +60,12 @@ function btnSwitcher() {
   }
 }
 
-// Detecting Keyboard Press
-document.addEventListener("keydown", function (keyEvent) {
-  keySwitcher(keyEvent.code);
-});
+function btnAnimation(currentKey) {
+  var activeBtn = document.querySelector("." + currentKey);
 
-function keySwitcher(keyEvent) {
-  switch (keyEvent) {
-    case "KeyW":
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
+  activeBtn.classList.add("pressed");
 
-    case "KeyA":
-      var tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-
-    case "KeyS":
-      var tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-
-    case "KeyD":
-      var tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-      break;
-
-    case "KeyJ":
-      var snare = new Audio("sounds/snare.mp3");
-      snare.play();
-      break;
-
-    case "KeyK":
-      var crash = new Audio("sounds/crash.mp3");
-      crash.play();
-      break;
-
-    case "KeyL":
-      var kickBass = new Audio("sounds/kick-bass.mp3");
-      kickBass.play();
-      break;
-
-    default:
-      console.log();
-  }
+  setTimeout(function () {
+      activeBtn.classList.remove("pressed");
+  }, 200);
 }
