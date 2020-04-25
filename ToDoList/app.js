257dd7c6c2822eb69bd0015db2a8a32d9a381da6
nemus <nemanjadjekic93@gmail.com>
2020-04-25 19:54:47 +0200

    Adder work route

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = [];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -23,13 +24,29 @@ app.get("/", function (req, res) {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  items.push(req.body.newItem);
-  res.redirect("/");
-  console.log(items);
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, function () {
