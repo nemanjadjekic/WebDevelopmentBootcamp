411be9d0e47c4eb2bc111fe6e4ebeaf4b576f7ff
nemus <nemanjadjekic93@gmail.com>
2020-04-25 22:20:53 +0200

    Updated header and footer and did some refactoring

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -2,11 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = [];
-let workItems = [];
+const items = [];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -14,21 +15,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
-  res.render("list", { listTitle: day, newListItems: items });
+  res.render("list", { listTitle: date.getDate(), newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "Work") {
     workItems.push(item);
@@ -44,9 +35,13 @@ app.get("/work", function (req, res) {
 });
 
 app.post("/work", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function () {
