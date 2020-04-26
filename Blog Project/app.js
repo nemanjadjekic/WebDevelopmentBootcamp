924448b8bbf989fb00e8073f2042665a85a2cbdf
nemus <nemanjadjekic93@gmail.com>
2020-04-26 19:12:51 +0200

    Challenges 18-20 completed

--- Blog Project/app.js
+++ Blog Project/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -53,8 +54,15 @@ app.get("/posts/:postName", function (req, res) {
   const reqTitle = req.params.postName;
 
   posts.forEach(function (post) {
-    if (reqTitle === post.title) {
+    const storedTitleLowCase = lodash.lowerCase(post.title);
+    const storedTitleKebabCase = lodash.kebabCase(storedTitleLowCase);
+    const storedTitle = storedTitleKebabCase;
+
+    if (reqTitle === storedTitle) {
       console.log("We have a match!");
+      res.render("post", {postPageTitle: post.title, postPageContent: post.content});
+    } else {
+      console.log("No match!");
     }
   });
 });
