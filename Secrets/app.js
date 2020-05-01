f527154d739bf2352a23576930e5e63ede7169bd
nemus <nemanjadjekic93@gmail.com>
2020-05-01 14:53:55 +0200

    Level 3 - Hashing passwords

--- Secrets/app.js
+++ Secrets/app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+var md5 = require("md5");
 
 const app = express();
 
@@ -55,7 +56,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save(function (err) {
@@ -69,7 +70,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
