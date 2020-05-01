447fd42586c03c2f6a86ad14b69ae58057547430
nemus <nemanjadjekic93@gmail.com>
2020-05-01 14:36:18 +0200

    Level 2 - Database Encryption

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,11 +1,15 @@
 //jshint esversion:6
 
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.set("view engine", "ejs");
 
@@ -27,10 +31,12 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useFindAndModify: false,
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -65,12 +71,12 @@ app.post("/login", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser) {
-    if(err) {
+  User.findOne({ email: username }, function (err, foundUser) {
+    if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if(foundUser.password === password) {
+        if (foundUser.password === password) {
           res.render("secrets");
         }
       }
