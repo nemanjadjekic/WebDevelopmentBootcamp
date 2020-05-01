12d8f37140d5cb0ad433564d6d7c4b2cefbd3e9e
nemus <nemanjadjekic93@gmail.com>
2020-05-01 11:28:34 +0200

    Added Level 1: Username and pass auth

--- Secrets/app.js
+++ Secrets/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -20,6 +21,19 @@ app.use(
   })
 );
 
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", function (req, res) {
   res.render("home");
 });
@@ -30,6 +44,38 @@ app.get("/login", function (req, res) {
 
 app.get("/register", function (req, res) {
   res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser) {
+    if(err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if(foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
 });
 
 app.listen(3000, function () {
