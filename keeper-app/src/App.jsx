cf92f59f28ae2703ca3e367cebe4eaf6e8757cf5
nemus <nemanjadjekic93@gmail.com>
2020-05-03 10:09:07 +0200

    Did refactoring using map() and => functions

--- keeper-app/src/App.jsx
+++ keeper-app/src/App.jsx
@@ -1,30 +1,23 @@
-import React from "react";
+import React, { createContext } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "./Notes";
 
 function app() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
 }
 
 export default app;
-
-//1. Create a new React app. DONE
-//2. Create a App.jsx component. DONE
-//3. Create a Header.jsx component that renders a <header> element
-//to show the Keeper App name in an <h1>. DONE
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year. DONE
-
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
